c6276ec42d7ea895667a4c65366fbd688dc4f940
khanh27202 <73024784+khanh27202@users.noreply.github.com>
2022-05-08 20:56:49 -0400

    edit profile

---
+++ VacayToday/ProfileViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ProfileViewController.swift
+//  VacayToday
+//
+//  Created by Le Lan Khanh on 08/05/2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var userName: UILabel!
+    
+    @IBAction func onLogOut(_ sender: Any) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
